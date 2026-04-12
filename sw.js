721376abff08bb1656cb2770f6290e0c8296d5e7
hayato198609 <hayato1986924@gmail.com>
2026-04-12 23:54:15 +0900

    Implement service worker for caching assets

---
+++ sw.js
@@ -0,0 +1,36 @@
+const CACHE_NAME = 'trading-cal-v1';
+const ASSETS = ['./'];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k))
+      )
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.open(CACHE_NAME).then(cache =>
+      cache.match(event.request).then(cached => {
+        const networked = fetch(event.request)
+          .then(response => {
+            cache.put(event.request, response.clone());
+            return response;
+          })
+          .catch(() => cached);
+        return cached || networked;
+      })
+    )
+  );
+});
